50b63523b1d5cc8ecd9fd2b7ff59970cf56cefac
AlistairHeus <kartikjha13@gmail.com>
2022-05-30 23:43:50 +0530

    api

---
+++ pages/api/contact.js
@@ -0,0 +1,31 @@
+export default function (req, res) {
+  const dotenv = require("dotenv");
+  dotenv.config();
+
+  let nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+      user: "zephyr98k@gmail.com",
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    secure: true,
+  });
+
+  const mailData = {
+    from: "zephyr98k@gmail.com",
+    to: "info@atcgroup.co.in",
+    subject: `Message From ${req.body.name}`,
+    text: req.body.message + " | Sent from: " + req.body.email,
+    html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`,
+  };
+
+  transporter.sendMail(mailData, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+
+  console.log(req.body);
+  res.send("success");
+}
